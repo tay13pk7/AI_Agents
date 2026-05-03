f65e1aa2b2ad9b5c28b361dc84d5bfeb65ba625c
Prasad Kute <prasadkute7@gmail.com>
2026-05-03 16:39:09 +0530

    gpt

--- LLM_Model.py
+++ LLM_Model.py
@@ -7,12 +7,11 @@ print("Hello AI Agents");
 
 print("test");
 
-# // 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
 
 from openai import OpenAI
 
 client = OpenAI(
-    api_key="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"   # 👈 paste your key directly
+    api_key="?"
 )
 
 response = client.responses.create(
